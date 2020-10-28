bef6630114038991f15b1a48ec5c49f255486440
wangzhiyong04 <wangzhiyong04@baidu.com>
2020-10-28 18:36:26 +0800

    add licences to files

--- MMLCore/MML/iOS/Sources/Backend/PaddleCPU.h
+++ MMLCore/MML/iOS/Sources/Backend/PaddleCPU.h
@@ -1,10 +1,17 @@
+// Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
 //
-//  PaddleCPUOC.h
-//  MML
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by Baidu. Co.,Ltd. on 2020/2/12.
-//  Copyright © 2020 Baidu. All rights reserved.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 
 #import <Foundation/Foundation.h>
 
--- MMLCore/MML/iOS/Sources/Utils/MMLMachineCommonHeader.h
+++ MMLCore/MML/iOS/Sources/Utils/MMLMachineCommonHeader.h
@@ -1,10 +1,17 @@
+// Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
 //
-//  MMLMachineCommonHeader.h
-//  MML+Plugin
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by Baidu. Co.,Ltd. on 2019/11/14.
-//  Copyright © 2019 Baidu. All rights reserved.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 
 /// Machine的公共头文件
 #ifndef MMLMachineCommonHeader_h
